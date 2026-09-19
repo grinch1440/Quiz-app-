db92ee763e83c2b71433e085b39d031db5d60a12
grinch1440 <grinch1440@gmail.com>
2026-09-19 12:52:00 +0100

    Create main.js

---
+++ scr/main.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import QuizMaster from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <QuizMaster />
+  </React.StrictMode>
+);
